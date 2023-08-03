f3bd544d5f73fe6f15a53cef501381c8078f932d
yair <yair99@gmail.com>
2023-08-03 20:32:01 +0300

    rel01b

--- askme.py
+++ askme.py
@@ -4,33 +4,47 @@ from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
 
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-
 import streamlit as st
 from langchain.llms.openai import OpenAI
 
 from llama_index import (
     PromptHelper,
+    load_index_from_storage,
     ServiceContext,
+    StorageContext,
     SimpleDirectoryReader,
     VectorStoreIndex,
 )
 from llama_index.llms import ChatMessage, OpenAI
 
 service_context = ServiceContext.from_defaults(
-    llm=OpenAI(model="gpt-3.5-turbo", temperature=0)
+    #todo repalce with llamma2
+    llm=OpenAI(model="gpt-3.5-turbo", temperature=0) 
 )
+
 # messages = [
 #     ChatMessage(role="system", content="You are a pirate with a colorful personality"),
 #     ChatMessage(role="user", content="What is your name"),
 # ]
-max_input_size = 4096
-num_output = 256
-max_chunk_overlap = 20
-prompt_helper = PromptHelper(max_input_size, num_output)
+# max_input_size = 4096
+# num_output = 256
+# max_chunk_overlap = 20
+# prompt_helper = PromptHelper(max_input_size, num_output)
 
-data = SimpleDirectoryReader(input_dir="C:\dev\llama_index\examples\dk").load_data()
-index = VectorStoreIndex.from_documents(data, service_context=service_context)
+
+# storage_context = StorageContext.from_defaults()
+# data = SimpleDirectoryReader(input_dir="C:\\dev\\llama_index\\examples\\dk").load_data()
+# index = VectorStoreIndex.from_documents(
+#     data, 
+#     service_context=service_context,         
+#     storage_context=storage_context, 
+#     show_progress=True
+# )
+# storage_context.persist(persist_dir=f'./storage/myth')
+
+
+storage_context = StorageContext.from_defaults(persist_dir=f'./storage/myth')
+index = load_index_from_storage(storage_context=storage_context)
 
 # Configure chat engine
 chat_engine = index.as_chat_engine(
@@ -38,9 +52,17 @@ chat_engine = index.as_chat_engine(
 )  # react , best or context ?
 
 # Define a simple Streamlit app
-st.title("Ask Denisa")
+
+st.set_page_config(
+	layout="centered",  # Can be "centered" or "wide". In the future also "dashboard", etc.
+	initial_sidebar_state="auto",  # Can be "auto", "expanded", "collapsed"
+	page_title="denisaBot",  # String or None. Strings get appended with "• Streamlit". 
+	page_icon="fav.png",  # String, anything supported by st.image, or None.
+)
+
+st.title('What would you like to ask the book "Algorithms and Automation"?')
 query = st.text_input(
-    "What would you like to ask? (source: Algorithms and Automation)", ""
+     'Examples: "What is a prototype, and how is it relevant to philosophical discussions?"', ""
 )
 
 # If the 'Submit' button is clicked
